ae7c7e78901870c28bc255f8add6d24977aff526
Rafal Zarajczyk <rzarajczyk@box.com>
2022-04-28 22:45:10 +0200

    Initial commit.

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from homie_helpers import MqttSettings
+
+from PhilipsHue import PhilipsHue
+from bootstrap.bootstrap import start_service
+
+config, logger, timezone = start_service()
+
+scheduler = BlockingScheduler(timezone=timezone)
+
+device = PhilipsHue(config=config['sony-bravia'], mqtt_settings=MqttSettings.from_dict(config['mqtt']))
+
+scheduler.add_job(device.refresh, 'interval', seconds=config['fetch-interval-seconds'])
+
+scheduler.start()
